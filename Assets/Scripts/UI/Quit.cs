589241a4ff351c10f6589945d4c92e2c510c076d
TangYucao <812015941@qq.com>
2019-12-04 22:44:33 -0800

    player

---
+++ Assets/Scripts/UI/Quit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quit : MonoBehaviour
+{
+    public static Quit instance;
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+    public void OnClick()
+    {
+        Application.Quit();
+    }
+
+}
